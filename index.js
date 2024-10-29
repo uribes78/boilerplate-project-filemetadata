4817c714402a3f211a971e644c68c244ec224591
Saul Uribe <s4ulu4@gmail.com>
2024-10-29 16:39:33 -0700

    Upload process

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
 
-var app = express();
+const app = express();
+const upload = multer({dest: './uploads/'});
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +13,14 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+    if (req.file) {
+        let { originalname: name, mimetype: type, size } = req.file;
+        res.status(200).json({ name, type, size }).end();
+    } else {
+        res.status(400).json({error: 'No file found'}).end();
+    }
+});
 
 
 const port = process.env.PORT || 3000;
